5bfcb550d1b48e71466ef3c4c5eb2fea4434919f
Eric <217014110@stu.vtc.edu.hk>
2022-06-06 05:16:26 +0800

    feat: add user routes with semantic status code

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,27 @@
-var express = require('express');
-var router = express.Router();
+const Router = require('koa-router')
+const model = require('../models/users')
+const can = require('../permission/user-permission')
+const auth = require('../controllers/authentication')
+const router = Router({prefix: '/api/v1/users'})
+const { StatusCode } = require('status-code-enum')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
 
-module.exports = router;
+router.get('/', auth, getAll)
+
+async function getAll(ctx) {
+  try {
+    const permission = can.readAll(ctx.state.user)
+    if (!permission.granted) {
+      ctx.status = StatusCode.ClientErrorForbidden;
+    } else {
+      const result = await model.getAll()
+      if (result.length) {
+        ctx.body = result;
+      }
+    }
+  } catch (e) {
+    ctx.status = StatusCode.ClientErrorBadRequest
+  }
+}
+
+module.exports = router

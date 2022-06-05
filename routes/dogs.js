53c5a58b8a22dbd550de41430c9d836510b17c1e
Eric <217014110@stu.vtc.edu.hk>
2022-06-06 07:56:44 +0800

    refactor: refactor fetch dogs api from external

--- routes/dogs.js
+++ routes/dogs.js
@@ -1,9 +1,22 @@
-var express = require('express');
+const Router = require('koa-router')
+const bodyParser = require('koa-bodyparser')
 const request = require('request')
-var router = express.Router();
+const model = require("../models/petfindings");
+const StatusCode = require("status-code-enum");
+const router = Router({prefix: '/api/v1/dogs'})
 
-/* GET users listing. */
-router.get('/', function (req, res, next) {
+/**
+ * GET dog breeds.
+ */
+router.get('/', getAll)
+
+/**
+ * Get all dogs breed from Dogs API.
+ * @async
+ * @param ctx - request context
+ * @param next - next handler
+ */
+async function getAll(ctx, next) {
     const fetchDogBreeds = (url) => new Promise((resolve, reject) => {
         request(url, (err, res, body) => {
             if (err) reject(new Error('invalid API call'))
@@ -11,7 +24,14 @@ router.get('/', function (req, res, next) {
         })
     })
 
-    fetchDogBreeds('https://dog.ceo/api/breeds/list/all').then(data => res.send(data))
-})
+    try {
+        fetchDogBreeds('https://dog.ceo/api/breeds/list/all').then(data => {
+            ctx.status = StatusCode.SuccessOK
+            ctx.body = {'data': data, 'status': 'ok'}}
+        )
+    } catch (e) {
+        ctx.status = StatusCode.ClientErrorBadRequest
+    }
+}
 
 module.exports = router;

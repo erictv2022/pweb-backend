19a66c59a22460e7759c282a180b297aa5aba749
Eric <217014110@stu.vtc.edu.hk>
2022-06-06 12:52:26 +0800

    other: update project run env

--- app.js
+++ app.js
@@ -7,26 +7,6 @@ const logger = require('morgan');
 
 const app = new Koa();
 
-// view engine setup
-// app.use(logger('dev'));
-// app.use(cookieParser());
-
-// catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
-
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
-
 //routes
 const petfindings = require('./routes/petfindings')
 app.use(petfindings.routes())
@@ -45,4 +25,7 @@ app.on('error', (err, ctx) => {
     logger.error('server error', err, ctx)
 });
 
-module.exports = app;
+let port = process.env.PORT || 10888;
+
+app.listen(port)
+console.log('API is ready')

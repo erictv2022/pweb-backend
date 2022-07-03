2ccdf11fdd8f14545adab3667801171a54a76e93
Eric <217014110@stu.vtc.edu.hk>
2022-07-04 03:08:44 +0800

    fix: petfinding data seed

--- sql/postgreSQL/petfinding.sql
+++ sql/postgreSQL/petfinding.sql
@@ -18,3 +18,25 @@ CREATE TABLE public.petfindings
     userid       int4          NULL,
     CONSTRAINT petfindings_pkey PRIMARY KEY (id)
 );
+
+INSERT INTO public.petfindings
+    (breed, subbreed, shelter, summary, imageurl, published, userid)
+VALUES ('affenpinscher', null, 'Downtown London Center 1', '1 years-old',
+        'https://images.dog.ceo/breeds/affenpinscher/n02110627_7404.jpg', true, 1),
+       ('african', null, 'Downtown London Center 2', '2 years-old',
+        'https://images.dog.ceo/breeds/african/n02116738_9748.jpg', true, 1),
+       ('australian', 'shepherd', 'Downtown London Center 2', '3 years-old', null, true, 1),
+       ('basenji', null, 'Downtown Manchester Center 1', 'brown, 1 years-old',
+        'https://images.dog.ceo/breeds/basenji/n02110806_759.jpg', true, 1),
+       ('buhund', 'norwegian', 'Downtown Manchester Center 2', 'recently found', null, true, 1),
+       ('african', null, 'Downtown London Center 2', '2 years-old',
+        'https://images.dog.ceo/breeds/african/n02116738_9748.jpg', true, 1),
+       ('bulldog', 'boston', 'Center 1, Coventry', 'small size, around 6 months', null, true, 1),
+       ('bulldog', 'english', 'Center 1, Coventry', 'senior age, short hair', null, true, 1),
+       ('bulldog', 'french', 'Center 1, Coventry', 'active, around 3 years-old', null, true, 1),
+       ('poodle', 'toy', 'Center 2, Birmingham', 'white in color',
+        'https://images.dog.ceo/breeds/poodle-toy/n02113624_2905.jpg', true, 1),
+       ('poodle', 'miniature', 'Center 4, Birmingham', 'brown hair, 30cm height',
+        'https://images.dog.ceo/breeds/poodle-miniature/n02113712_212.jpg', true, 1),
+       ('terrier', 'fox', 'Downtown London Center 2', 'mix color white and brown',
+        'https://images.dog.ceo/breeds/terrier-fox/n02095314_3372.jpg', true, 1)

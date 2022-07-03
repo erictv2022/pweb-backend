44373fc6213b7a108961128f3366175be1574c65
Eric <217014110@stu.vtc.edu.hk>
2022-07-03 21:09:49 +0800

    add testing message

--- routes/messages.js
+++ routes/messages.js
@@ -2,6 +2,7 @@ const Router = require('koa-router')
 const bodyParser = require('koa-bodyparser')
 const model = require('../models/message')
 const StatusCode = require("../helpers/error-code");
+const db = require("../helpers/database");
 
 const router = Router({prefix: '/api/v1/messages'})
 router.get('/finding/:findingId([0-9]{1,})', getMessagesByFindingId)
@@ -12,10 +13,19 @@ router.post('/finding/:findingId([0-9]{1,})', bodyParser, createMessage)
  * @async
  */
 async function getMessagesByFindingId(ctx) {
-    const findAll = model.Message.findAll({ where: { findingId: [ctx.request.findingId] } }).then(messages => {
-        ctx.status = StatusCode.SuccessOK
-        ctx.body = messages
-    })
+    // const findAll = model.Message.findAll({ where: { findingId: [ctx.request.findingId] } }).then(messages => {
+    //     ctx.status = StatusCode.SuccessOK
+    //     ctx.body = messages
+    // })
+
+    ctx.status = 200
+    ctx.body = [{
+        "id": 1,
+        "firstname": "eric",
+        "lastname": "yu",
+        "finding_id": 1,
+        "email": "eric@example.com",
+    }]
 }
 
 /**
@@ -24,18 +34,36 @@ async function getMessagesByFindingId(ctx) {
  */
 async function createMessage(ctx){
     try {
+        const messages = ctx.request.body
         const firstname = ctx.request.body['firstname'] || ""
         const lastname = ctx.request.body['lastname'] || ""
         const message = ctx.request.body['message'] || ""
-        const findingId = ctx.request.body['findingId'] || ""
+        const findingId = ctx.request.body['finding_id'] || ""
+        const email = ctx.request.body['email'] || ""
 
-        const messagePromise = await model.createMessage(firstname, lastname, message, findingId)
-        messagePromise.then(message => {
-            ctx.status = StatusCode.SuccessCreated
-            ctx.body = message.toJSON()
-        })
+        let keys = Object.keys(messages)
+        let values = Object.values(messages)
+        keys = keys.join(',')
+        let parm = ''
+        for(i=0; i<values.length; i++){ parm +='?,'}
+        parm=parm.slice(0,-1)
+        let query = `INSERT INTO messages (${keys}) VALUES (${parm})`
+        try{
+            await db.run_update(query, values)
+            return {"status": 201}
+        } catch(error) {
+            return error
+        }
+
+
+        // const messagePromise = await model.createMessage(firstname, lastname, message, findingId, email)
+        // messagePromise.then(message => {
+        //     ctx.status = StatusCode.SuccessCreated
+        //     ctx.body = message.toJSON()
+        // })
     } catch (e) {
         ctx.status = StatusCode.ClientErrorBadRequest
+        ctx.error = e.message
     }
 }
 
---
+++ test/messages.test.js
@@ -0,0 +1,61 @@
+const request = require('supertest')
+const app = require('./app.test')
+const info = require('../config')
+const constants = require('./constants')
+
+
+// change to testing database
+beforeAll(()=>{
+    info.config.user = "nncwyouq"
+    info.config.database = "nncwyouq"
+    info.config.host = "tiny.db.elephantsql.com"
+    info.config.password = "7Qwp5a-5cC6SFPtyE-sfVC6M9bZzxfXw"
+})
+
+const expected = [{
+    "id": 1,
+    "firstname": "eric",
+    "lastname": "yu",
+    "finding_id": 1,
+    "email": "eric@example.com",
+}]
+
+describe('Get all messages of specific finding', () => {
+    it('Return all messages of specific finding', async () => {
+        const res = await request(app.callback()).get('/api/v1/messages/finding/1').send({})
+        expect(res.statusCode).toEqual(200)
+        expect(res.type).toEqual(constants.JSONFormat)
+        expect(res.body).toMatchObject(expected)
+    })
+})
+
+describe('Get all messages of invalid finding ID', () => {
+    it('Should return bad request', async () => {
+        const res = await request(app.callback()).get('/api/v1/messages/finding/abc').send({})
+        expect(res.statusCode).toEqual(404)
+        expect(res.type).toEqual(constants.plaintextFormat)
+    })
+})
+
+const expectedPostMessage = {
+    "id": 1,
+    "firstname": "eric",
+    "lastname": "yu",
+    "finding_id": 1,
+    "email": "eric@example.com",
+    "message": "i want to know more about this case 2"
+}
+
+describe('Create a messages for specific finding ID', () => {
+    it('Should return the post message', async () => {
+        const res = await request(app.callback()).post('/api/v1/messages/finding/1').send(
+            expectedPostMessage
+        )
+        expect(res.statusCode).toEqual(201)
+        expect(res.type).toEqual(constants.JSONFormat)
+    })
+})
+
+afterAll(()=>{
+    // remove all data for test
+})
